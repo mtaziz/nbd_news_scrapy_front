aa558dbd0be1241366fe0aeed27a7c7c18254136
monkeyzhu <monkeyzhu333@gmail.com>
2017-07-10 10:45:33 +0800

    add post host

--- nbd_news_scrapy_front/settings.py
+++ nbd_news_scrapy_front/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qpb75ckpd=swk0=+*v!c^nalw9!9kj(r58$q5@l)3rgr*-68z5'
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['news.nbd.com.cn']
+ALLOWED_HOSTS = ['news.nbd.com.cn', 'nbd-news-scrapy-front']
 
 # Application definition
 
