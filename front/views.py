d18d47ea8630e20b2636ed20a513a9970de515ba
monkeyzhu <monkeyzhu333@gmail.com>
2017-07-14 19:48:11 +0800

    change article tag

--- front/views.py
+++ front/views.py
@@ -45,7 +45,7 @@ def receive_article_result(request):
             id=updated_values['article_for_crawl_media_sort'])
         updated_values['article_for_crawl_dir_sort'] = CrawlDirSort.objects.get(
             id=updated_values['article_for_crawl_dir_sort'])
-
+        print updated_values
         if article_info.is_valid():
             article_info.save()
             return HttpResponse(json.dumps({'status': 200, 'message': 'success'}), content_type="application/json")
