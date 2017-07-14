383ef7f4a53f92190134c143efef12b1507afc67
monkeyzhu <monkeyzhu333@gmail.com>
2017-07-14 19:45:58 +0800

    change article tag

--- front/views.py
+++ front/views.py
@@ -41,10 +41,10 @@ def receive_article_result(request):
         for key, value in request.POST.items():
             updated_values[key] = value
 
-        # updated_values['article_for_crawl_media_sort'] = CrawlMediaSort.objects.get(
-        #     id=updated_values['article_for_crawl_media_sort'])
-        # updated_values['article_for_crawl_dir_sort'] = CrawlDirSort.objects.get(
-        #     id=updated_values['article_for_crawl_dir_sort'])
+        updated_values['article_for_crawl_media_sort'] = CrawlMediaSort.objects.get(
+            id=updated_values['article_for_crawl_media_sort'])
+        updated_values['article_for_crawl_dir_sort'] = CrawlDirSort.objects.get(
+            id=updated_values['article_for_crawl_dir_sort'])
 
         if article_info.is_valid():
             article_info.save()
