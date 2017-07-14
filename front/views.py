231f518e47e0eca6653386df9bcccd714d01eae4
monkeyzhu <monkeyzhu333@gmail.com>
2017-07-14 19:32:53 +0800

    change article tag

--- front/views.py
+++ front/views.py
@@ -50,7 +50,7 @@ def receive_article_result(request):
             article_info.save()
             return HttpResponse(json.dumps({'status': 200, 'message': 'success'}), content_type="application/json")
         else:
-            return article_info
+            return HttpResponse(article_info)
             # print article_info.errors
     else:
         return HttpResponse(json.dumps({'status': 500, 'message': 'failed'}), content_type="application/json")
