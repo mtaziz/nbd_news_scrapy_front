73d20872d6c7c7055165ddd2d30d05e0d0371c17
monkeyzhu <monkeyzhu333@gmail.com>
2017-07-17 11:04:12 +0800

    add detail page

--- front/urls.py
+++ front/urls.py
@@ -7,7 +7,6 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     url(r'^search/', login_required(KeyWordSearchView.as_view(), login_url="/admin/login/"), name='haystack_search'),
     url(r'^$', views.index, name='index'),
-    url(r'^test', views.test, name='test'),
     url(r'^receive_article_result', views.receive_article_result, name='receive_article_result'),
     url(r'^get_media_sorts', views.get_media_sorts, name='get_media_sorts'),
     url(r'^get_dir_sorts', views.get_dir_sorts, name='get_dir_sorts'),
