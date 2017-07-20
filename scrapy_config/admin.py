81c03f8bac3e219691f71359a7033c18a2cf0719
monkeyzhu <monkeyzhu333@gmail.com>
2017-07-20 09:14:37 +0800

    add search to scrapy_config model

--- scrapy_config/admin.py
+++ scrapy_config/admin.py
@@ -23,7 +23,7 @@ batch_open.short_description = u'批量开启抓取'
 class CrawlDirSortAdmin(admin.ModelAdmin):
     list_display = ('crawl_dir_sort_name', 'crawl_push_status_plus')
     list_per_page = 20
-    search_fields = ('crawl_dir_sort_name')
+    search_fields = ('crawl_dir_sort_name',)
     actions = ['update_status_to_on', 'update_status_to_off']
 
     def update_status_to_on(self, request, obj=None):
@@ -58,7 +58,7 @@ class CrawlMediaAdmin(admin.ModelAdmin):
 class CrawlMediaSortAdmin(admin.ModelAdmin):
     list_display = ('id', 'crawl_media_sort_name')
     list_per_page = 20
-    search_fields = ('crawl_media_sort_name')
+    search_fields = ('crawl_media_sort_name',)
 
 
 class AllSiteCrawlConfigAdmin(admin.ModelAdmin):
@@ -66,7 +66,7 @@ class AllSiteCrawlConfigAdmin(admin.ModelAdmin):
         'crawl_media', 'crawl_start_url', 'crawl_link_extractor_allow', 'crawl_link_extractor_deny',
         'crawl_xpath_rule_set', 'crawl_note', 'crawl_frequency', 'crawl_scrapyd_jobid', 'updated_at')
     list_per_page = 50
-    search_fields = ('crawl_start_url')
+    search_fields = ('crawl_start_url',)
     actions = [batch_open, batch_close]
 
 
@@ -74,7 +74,7 @@ class OnePageCrawlConfigAdmin(admin.ModelAdmin):
     list_display = (
         'crawl_start_url', 'crawl_media_sort', 'crawl_frequency', 'crawl_status_fri', 'updated_at')
     list_per_page = 50
-    search_fields = ('crawl_start_url')
+    search_fields = ('crawl_start_url',)
     actions = [batch_open, batch_close]
 
     def crawl_status_fri(self, obj):
@@ -92,7 +92,7 @@ class JsonCrawlConfigAdmin(admin.ModelAdmin):
     list_display = (
         'crawl_start_url', 'crawl_media_sort', 'crawl_next_url', 'crawl_frequency', 'crawl_status_fri')
     list_per_page = 50
-    search_fields = ('crawl_start_url')
+    search_fields = ('crawl_start_url',)
     actions = [batch_open, batch_close]
 
     def crawl_status_fri(self, obj):
@@ -111,7 +111,7 @@ class NextPageCrawlConfigAdmin(admin.ModelAdmin):
         'crawl_start_url', 'crawl_media', 'crawl_media_sort', 'crawl_dir_sort', 'crawl_xpath_rule_set',
         'crawl_note', 'crawl_frequency', 'crawl_scrapyd_jobid', 'crawl_status_fri', 'updated_at')
     list_per_page = 50
-    search_fields = ('crawl_start_url')
+    search_fields = ('crawl_start_url',)
     actions = [batch_open, batch_close]
 
     def crawl_status_fri(self, obj):
