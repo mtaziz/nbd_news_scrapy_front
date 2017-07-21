31d7d4648dba5656b4ed0b56c3a5e861c442502f
monkeyzhu <monkeyzhu333@gmail.com>
2017-07-21 18:26:22 +0800

    update  api para

--- user_profile/views.py
+++ user_profile/views.py
@@ -6,6 +6,7 @@ from django.contrib import auth
 import json
 from forms import UserForm, UserProfileForm
 from django.contrib import messages
+from scrapy_config.models import CrawlMedia, CrawlMediaSort, CrawlDirSort
 from django.core import serializers
 
 
@@ -31,6 +32,32 @@ def favorite(request):
     user_favorite_info['user_favorite_crawl_media_sort'] = current_user_profile.user_favorite_crawl_media_sort
     user_favorite_info['user_favorite_crawl_media'] = current_user_profile.user_favorite_crawl_media
     user_favorite_info['user_favorite_crawl_dir_sort'] = current_user_profile.user_favorite_crawl_dir_sort
+    media_sort_list = []
+    for i in user_favorite_info['user_favorite_crawl_media_sort'].split(","): # i = ["6_get_medias","21_get_medias","16_get_medias"]
+        media_sort_info = CrawlMediaSort.objects.filter(id=i.split('_')[0]).values('crawl_media_sort_name')
+        media_sort_list.append({
+            'name': media_sort_info['crawl_media_sort_name'],
+            'id': str(i.split('_')[0]) + "_get_media_sorts"
+        })
+
+    media_list = []
+    for i in user_favorite_info['user_favorite_crawl_media'].split(","):
+        media_info = CrawlMedia.objects.filter(id=i.split('_')[0]).values('crawl_media_name')
+        media_list.append({
+            'name': media_info['crawl_media_name'],
+            'id': str(i.split('_')[0]) + "_get_medias"
+        })
+
+    media_dir_sort_list = []
+    for i in user_favorite_info['user_favorite_crawl_dir_sort'].split(","):
+        media_info = CrawlDirSort.objects.filter(id=i.split('_')[0]).values('crawl_dir_sort_name')
+        media_dir_sort_list.append({
+            'name': media_info['crawl_dir_sort_name'],
+            'id': str(i.split('_')[0]) + "_get_dir_sorts"
+        })
+    user_favorite_info['user_favorite_crawl_media_sort'] = media_sort_list
+    user_favorite_info['user_favorite_crawl_media'] = media_list
+    user_favorite_info['user_favorite_crawl_dir_sort'] = media_dir_sort_list
     return HttpResponse(json.dumps(user_favorite_info), content_type="application/json")
 
 
